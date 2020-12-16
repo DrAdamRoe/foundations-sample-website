df8e6bc7498f68556665f50897b6380d1ec219c2
Adam Roe <adam.j.roe@gmail.com>
2020-12-16 16:06:39 +0100

    update folder and file structure, imports and paths

--- sample_website/sample_website.py
+++ sample_website/sample_website.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Flask!'
 
+
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
-
--- sample_website/tests/test_hello_world.py
+++ sample_website/tests/test_hello_world.py
@@ -1,5 +1,6 @@
-from flasky import hello_world
+from sample_website.sample_website import hello_world
 
 
 def test_response():
     assert hello_world() == 'Hello, Flask!'
+
