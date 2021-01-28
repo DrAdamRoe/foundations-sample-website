5627cafaf90ae085b1594bcf47de1d8b15006551
Fabian Volkers <fabian.volkers@gmail.com>
2021-01-28 13:50:27 +0100

    add test route

--- sample_website/sample_website.py
+++ sample_website/sample_website.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Flask!'
 
+@app.router('/test')
+def test_route():
+    return 'Test Route'
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
