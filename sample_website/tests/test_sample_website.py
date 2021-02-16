1661e4bdde47ca1bca21338ec39952de6d48ccd0
Adam Roe <adam.j.roe@gmail.com>
2021-02-16 14:59:37 +0100

    add empty lines (PEP8)

--- sample_website/tests/test_sample_website.py
+++ sample_website/tests/test_sample_website.py
@@ -3,4 +3,3 @@ from sample_website.website import hello_world
 
 def test_sample_website():
     assert hello_world() == '<h1>Hello, Flask!</h1>'
-
--- week2/tests/test_week2.py
+++ week2/tests/test_week2.py
@@ -3,4 +3,3 @@ from week2.sample_website import hello_world
 
 def test_week2():
     assert hello_world() == '<h1>Hello, Week 2!</h1>'
-
