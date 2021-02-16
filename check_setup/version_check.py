2cd19aedf8b741ceac44591ac776aee8bf3b1a87
Adam Roe <adam.j.roe@gmail.com>
2021-02-16 14:46:17 +0100

    message works well on python2 and pytho3 now

--- check_setup/version_check.py
+++ check_setup/version_check.py
@@ -1,8 +1,11 @@
 import sys
 
 if sys.version_info < (3, 8):
-    print("########### oh no! ##########")
-    print("You need Python 3.8 or higher. You are using version %i.%i \n" % (sys.version_info.major,sys.version_info.minor ))
+    message = """ 
+    ########### oh no! ##########
+    You need Python 3.8 or higher. You are using version %i.%i 
+    """ % (sys.version_info.major,sys.version_info.minor)
+    print(message)
     sys.exit(1)
 
 print("You are using a comptabile verison of Python, good job!")
