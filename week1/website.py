e5b06a07346b88e14a12b90f5c97309e3b651cc5
Adam Roe <adam.j.roe@gmail.com>
2021-02-19 10:05:52 +0100

    add base and first templated page

--- week1/website.py
+++ week1/website.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/first-page')
+def first_page():
+    return render_template('first-page.html', page_title="First Template Page")
+
+
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
