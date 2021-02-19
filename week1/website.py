0924986158ce07fafdb91035430fc976db1d5325
Adam Roe <adam.j.roe@gmail.com>
2021-02-19 09:46:30 +0100

    boilerplate for week1

--- week1/website.py
+++ week1/website.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, Week 1!</h1>'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
