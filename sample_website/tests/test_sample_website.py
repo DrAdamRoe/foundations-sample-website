17925cddada00701341ef749789dc636d8fad950
NlNOVATlON <ninovationlab@gmail.com>
2021-02-21 17:24:51 +0100

    Sample website edited back to original message

--- sample_website/tests/test_sample_website.py
+++ sample_website/tests/test_sample_website.py
@@ -2,4 +2,4 @@ from sample_website.website import hello_world
 
 
 def test_sample_website():
-    assert hello_world() == '<h1>Hello, Class!</h1>'
+    assert hello_world() == '<h1>Hello, Flask!</h1>'
--- sample_website/website.py
+++ sample_website/website.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Class!</h1>'
+    return '<h1>Hello, Flask!</h1>'
 
 
 if __name__ == "__main__":
