f0c13c62b0ef3f34d0e9d994b38abc0b42c1ad0b
Adam Roe <adam.j.roe@gmail.com>
2021-02-25 08:51:50 +0100

    improve instructions

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -2,6 +2,9 @@
 # This function should take one argument, a color name,
 # and it should return one argument, the hex code of the color,
 # if that color exists in our data. If it does not exist, you should
+# raise and handle an error that helps both you as a developer,
+# for example by logging the request and error, and the user,
+# letting them know that their color doesn't exist.
 
 def get_color_code(color_name):
     # this is where you should add your logic to check the color.
