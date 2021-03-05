5b95224c1fb093c7880f88002a2a6425a734b833
LennyS <Lennart.Stachowiak@code.berlin>
2021-03-05 16:43:25 +0100

    Task finished

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -1,3 +1,5 @@
+import json
+
 # This file should contain a function called get_color_code().
 # This function should take one argument, a color name,
 # and it should return one argument, the hex code of the color,
@@ -6,11 +8,18 @@
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
 
+
 def get_color_code(color_name):
     # this is where you should add your logic to check the color.
     # Open the file at data/css-color-names.json, and return the hex code
     # The file can be considered as JSON format, or as a Python dictionary.
+    """ Opens the json file and load it to a Python dict """
+    with open('/Users/lennartstachowiak/SE/foundations/foundations-sample-website/color_check/data/css-color-names.json') as f:
+        color_list = json.load(f)
 
-    hex_code = '#0000ff'
-
-    return hex_code
+    if color_name in color_list:
+        """ Checks if color in list and save it in hex_code """
+        hex_code = color_list[color_name]
+        return hex_code
+    else:
+        return None
--- color_check/website.py
+++ color_check/website.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+import logging
 from color_check.controllers.get_color_code import get_color_code
 app = Flask(__name__)
+
+logging.basicConfig(filename='color_check/log.txt',
+                    filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 @app.route('/')
@@ -19,13 +23,25 @@ def show_color():
     # - check if the color exists in our list, return the hex code if it does
     # - render a new page which shows a square of that color and its name
     # - if the color doesn't exist, give the user a useful error message.
-    # - create a log.txt file which records (logs) the user requests. 
+    # - create a log.txt file which records (logs) the user requests.
 
-    user_submitted_string = 'blue'
+    user_submitted_string = (request.form.get(
+        'color')).lower().replace(' ' '.', '')
     color_hex_code = get_color_code(user_submitted_string)
 
-    return render_template('color.html', page_title="Show Color",
-                           color_hex_code=color_hex_code)
+    # Creating two sentences for color available and not
+    color_text = "Your color is " + \
+        str(user_submitted_string) + ".\n" + \
+        "Your color code is " + str(color_hex_code)
+    no_color_text = "Your color isn't available"
+    logging.debug(user_submitted_string)
+
+    if color_hex_code == None:
+        return render_template('color.html', page_title="Show Color",
+                               message=no_color_text)
+    else:
+        return render_template('color.html', page_title="Show Color",
+                               message=color_text, color_hex_code=color_hex_code)
 
 
 if __name__ == "__main__":
