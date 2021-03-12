1c823636b604a26393ce11c867cfb5c16d6184b8
Adam Roe <adam.j.roe@gmail.com>
2021-03-12 11:48:45 +0100

    database connected

--- covid_app/website.py
+++ covid_app/website.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
 from flask import render_template
-from covid_app.controllers.add_meeting import add_meeting
+from covid_app.controllers.database_helpers import connect_to_database
+from covid_app.controllers.database_helpers import close_conection_to_database
+from covid_app.controllers.database_helpers import change_database
+from covid_app.controllers.database_helpers import query_database
+
+
 app = Flask(__name__)
+
+# This is an example of how to configure a flask.
+app.config['DATABASE_FILE'] = 'data/covid_app.sqlite'
 
 
 @app.route('/')
@@ -13,10 +21,33 @@ def index():
 def create_meeting():
     try:
         name = request.form.get('name')
-        add_meeting(name)
+        print("met:", name, "today")
+        # turn this into an SQL command
+        sql_insert = "INSERT INTO Meetings VALUES {name}".format(name=name)
+
+        # connect to the database with the filename configured above
+        # returning a 2-tuple that contains a connection and cursor object
+        # --> see file database_helpers for more
+        database_tuple = connect_to_database(app.config("DATABASE_FILE"))
+
+        # now that we have connected, add the new meeting (insert a row)
+        # --> see file database_helpers for more
+        change_database(database_tuple[0], database_tuple[1], sql_insert)
+
+        # now, get all of the meetings from the database, not just the new one.
+        # first, define the query to get all meetings:
+        sql_query = "FROM Meetings SELECT *"
+
+        # query the database, by passinng the database cursor and query,
+        # we expect a list of tuples corresponding to all rows in the database
+        query_response = query_database(database_tuple[1], sql_query)
+
+        close_conection_to_database(database_tuple[0])
+
         # In addition to HTML, we will respond with an HTTP Status code
         # The status code 201 means "created": a row was added to the database
-        return render_template('index.html', page_title="Covid Diary"), 201
+        return render_template('index.html', page_title="Covid Diary",
+                               meetings=query_response), 201
     except Exception:
         # something bad happended. Return an error page and a 500 error
         error_code = 500
